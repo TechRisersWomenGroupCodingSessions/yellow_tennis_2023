60163bca5d9b0f25e537a13d68ae1bc50a894e98
Emma Acquah <emmaacquah@gmail.com>
2023-08-30 19:45:24 +0100

    Added design comments, start of Players class, started to test Player names

--- tennis-game.js
+++ tennis-game.js
@@ -1,7 +1,15 @@
+const Player = require("./player");
+
 class Tennis {
-    constructor (players) {
-        this.players = players
-    };
+  constructor(players) {
+    this.players = players;
+    this.player1 = new Player(players[0]);
+    this.player2 = new Player(players[1]);
+  }
+  //   const player1 = new Player("John");
+  //   const player2 = new Player("Mary");
 }
+// const player1 = new Tennis("John");
+// const player2 = new Tennis("Mary");
 
 module.exports = Tennis;
--- tennis-game.test.js
+++ tennis-game.test.js
@@ -1,6 +1,26 @@
+// Comments:
+// Set up game
+// Players should take turns (alternate)
+// Test both players have had a turn - one go each (function)
+// Scores array - too much detail, stick to design not implementation
+// Set up counter variable for each player
+// increment counter variable up to 4 - then player should win
+
 const Tennis = require("./tennis-game");
+const Player = require("./player");
 
 test("There are 2 players in the game", () => {
-  const tennis = new Tennis(["John", "Mary"]);  
+  const tennis = new Tennis(["John", "Mary"]);
   expect(tennis.players.length).toBe(2);
 });
+
+test("Players have a name", () => {
+  const tennis = new Tennis(["John", "Mary"]);
+  expect(tennis.player1.name).toBe("John");
+  expect(tennis.player2.name).toBe("Mary");
+});
+
+// test("test both players have had a turn", () => {
+//   const tennis = new Tennis(["John", "Mary"]);
+//   expect(tennis.takeTurn()).toBe()
+// });
